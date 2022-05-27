16f7241a2b56cbe21e58595db1a91ca35ea08de2
Temur5319436 <temur5319436@gmail.com>
2022-05-28 00:33:01 +0500

    28 mayda o'rganilgan ishlarni saqlash

---
+++ 2/variables.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	//O'zgaruvchini e'lon qilish
+	var a int = 10
+	var b int = 2
+	fmt.Println("a + b = ", a+b)
+
+	//Bir nechta o'zgaruvchilari e'lon qilish
+	var c, d int = 10, 2
+	fmt.Println("c - d = ", c-d)
+
+	//O'zgaruvchilni qisqa ko'rinishda e'lom qilish
+	e := 10
+	f := 2
+	fmt.Println("e * f = ", e*f)
+
+	//Bir nechta o'zgaruvchilari qisqa ko'rinishda e'lom qilish
+	g, h := 10, 2
+	fmt.Println("g / h = ", g/h)
+}
