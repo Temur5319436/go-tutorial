438aa30606ee1a9e842eb18b5a109dd58bb5e45f
Temur5319436 <temur5319436@gmail.com>
2022-06-02 02:40:38 +0500

    Queue bo'limini ko'rib chiqdim

--- 10/queue.go
+++ 10/queue.go
@@ -7,13 +7,22 @@ import (
 
 func main() {
 	q := NewQueue()
-	fmt.Println(q)
 
+	q.push(64)
+	fmt.Println("queue elements:", q)
 	fmt.Println("is empty :", q.IsEmpty())
+
+	var next, _ = q.next()
+	fmt.Println("next element:", next)
 
 	q.push(3).push(8).push(2)
+	fmt.Println("queue elements:", q)
 	fmt.Println("is empty :", q.IsEmpty())
 
+	next, _ = q.shift()
+	fmt.Println("shifted element:", next)
+	fmt.Println("queue elements:", q)
+	fmt.Println("queue is empty:", q.IsEmpty())
 }
 
 type Queue struct {
@@ -28,18 +37,23 @@ func (q *Queue) IsEmpty() bool {
 	return len(q.data) == 0
 }
 
+func (q *Queue) next() (int, error) {
+	if q.IsEmpty() {
+		return 0, errors.New("Queue is empty !")
+	}
+	return q.data[0], nil
+}
+
 func (q *Queue) push(x int) *Queue {
 	q.data = append(q.data, x)
 	return q
 }
 
-func (q *Queue) next() (int, error) {
+func (q *Queue) shift() (int, error) {
 	if q.IsEmpty() {
-		return 0, errors.New("Navbat bo'sh")
+		return 0, errors.New("Queue is empty !")
 	}
-	return q.data[0], nil
-}
-
-func (q *Queue) shift() bool {
-	return true
+	x := q.data[0]
+	q.data = q.data[1:]
+	return x, nil
 }
